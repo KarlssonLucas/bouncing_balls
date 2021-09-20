0307638cfbb48c77af0dc5fd070745cca9bbd59e
Puzzel123 <jakobhenriksson@live.se>
2021-09-20 16:36:16 +0200

    Wheery

--- Model.java
+++ Model.java
@@ -25,23 +25,43 @@ class Model {
 		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2);
 		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3);
 	}
+	double gravity(Ball b, double g, double time){
+		return (-g * time * time)/2;
+	
+	}
 
 	void step(double deltaT) {
 		// TODO this method implements one step of simulation with a step deltaT
 		for (Ball b : balls) {
+			
 			// detect collision with the border
 			if (b.x < b.radius || b.x > areaWidth - b.radius) {
+				if (b.vx < 0){
+					b.x = b.radius;
+				} else{
+					b.x = areaWidth-b.radius;
+				}
+				
 				b.vx *= -1; // change direction of ball
 			}
 			if (b.y < b.radius || b.y > areaHeight - b.radius) {
+				if (b.vy <0){
+					b.y = b.radius;
+				} else{
+					b.y = areaHeight-b.radius;
+					b.vy -= (9.82*deltaT)/2;
+				}
 				b.vy *= -1;
 			}
-			
 			// compute new position according to the speed of the ball
 			b.x += deltaT * b.vx;
+			// (at^2)/2
+			b.vy += (-9.82 * deltaT)/2;	
+			// (s = vt + acceleration)		
 			b.y += deltaT * b.vy;
 		}
 	}
+
 	
 	/**
 	 * Simple inner class describing balls.
