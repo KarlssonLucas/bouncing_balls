dc0029500ab001d39891b17508a967f62781c6e1
Puzzel123 <jakobhenriksson@live.se>
2021-09-23 21:39:14 +0200

    helt ok

--- Model.java
+++ Model.java
@@ -23,12 +23,12 @@ class Model {
 		// Initialize the model with a few balls
 		balls = new Ball[2];
 		balls[0] = new Ball(width / 3, height * 0.7, 1, 2, 0.2);
-		balls[1] = new Ball(width / 3, height * 20, 0, 2, 0.22);
+		balls[1] = new Ball(width / 3, height * 20, -1, 2, 0.22);
 	}
 
 
 	void step(double deltaT) {
-		double g = 4;
+		double g = 0;
 		// TODO this method implements one step of simulation with a step deltaT
 		for (Ball b : balls) {
 			
@@ -66,55 +66,76 @@ class Model {
 			
 			
 		}
-
-		
 	}
-	void collide (Ball[] b){
-		if(b[0].radius + b[1].radius >= Math.abs(b[0].x - b[1].x) && b[0].radius + b[1].radius >= Math.abs(b[0].y - b[1].y)){
-			double angle = 0;
-
-            double deltax = Math.pow(b[0].x-b[1].x,2);
-            double deltay = Math.pow(b[0].y-b[1].y,2);
+	double getAngle(Ball[] b){
+		double angle = 0;
+        double deltax = Math.pow(b[0].x-b[1].x,2);
+        double deltay = Math.pow(b[0].y-b[1].y,2);
 
 			// Calculating angle
 			
-			angle = Math.acos(deltax/Math.sqrt((Math.pow(deltay,2) + Math.pow(deltax,2))));
+		angle = Math.acos(deltax/Math.sqrt((Math.pow(deltay,2) + Math.pow(deltax,2))));
 			
-			if(b[0].x > b[1].x  && b[0].y > b[1].y){
+		if(b[0].x > b[1].x  && b[0].y > b[1].y){
+			System.out.println("First if");
+			System.out.println("_______");
+			//Do nothing, base case
 				
 				//Do nothing, base case
 				
-			}
-			if(b[0].x < b[1].x  && b[0].y > b[1].y){
-				System.out.println(angle);
-				angle = Math.PI - angle;
-				System.out.println(angle);
-				System.out.println("_______");
-				
-			}
-			if(b[0].x > b[1].x  && b[0].y < b[1].y){
-				angle = -(Math.PI-angle);
-				
-			
-			}
-			if(b[0].x < b[1].x  && b[0].y < b[1].y){
-				System.out.println(angle);
-				angle = -angle;
-				System.out.println("_______");
-				//Do nothing, base case
-			}
+		}
 
+		if(b[0].x < b[1].x  && b[0].y > b[1].y){
+			System.out.println("Second if");
+			System.out.println(angle);
+			angle = Math.PI-angle;
+			
+			System.out.println(angle);
+			System.out.println("_______");
+				
+		}
+
+		if(b[0].x > b[1].x  && b[0].y < b[1].y){
+			System.out.println("Third if");
+			System.out.println(angle);
+			angle = -(Math.PI-angle);
+			System.out.println(angle);
+			System.out.println("_______");	
+						
+		}
+
+		if(b[0].x < b[1].x  && b[0].y < b[1].y){
+			System.out.println("Fourth if");
+			System.out.println(angle);
+			angle = -angle;
+			System.out.println(angle);
+			System.out.println("_______");
+		
+		}
+		return angle;
+	}
+	void rotate (Ball b, double angle){
+		double temp1 = b.vx;
+		double temp2 = b.vy;
+		b.vx = Math.cos(angle) * temp1 - Math.sin(angle) * temp2;
+		b.vy = Math.sin(angle) * temp1 + Math.cos(angle) * temp2;
+	}
+
+	void collide (Ball[] b){
+		if(b[0].radius + b[1].radius >= Math.abs(b[0].x - b[1].x) && b[0].radius + b[1].radius >= Math.abs(b[0].y - b[1].y)){
+			double angle = getAngle(b);
+			
 			//Rotate it
-            b[0].vx = Math.cos(angle) * b[0].vx - Math.sin(angle) * b[0].vy;
-            b[0].vy = Math.sin(angle) * b[0].vx + Math.cos(angle) * b[0].vy;
-            b[1].vx = Math.cos(angle) * b[1].vx - Math.sin(angle) * b[1].vy;
-            b[1].vy = Math.sin(angle) * b[1].vx + Math.cos(angle) * b[1].vy;
+			
+			rotate(b[0], angle);
+			rotate(b[1], angle);
 
 			// New velocity
 			double newb0vx = ((b[0].weight*b[0].vx - b[1].weight*b[0].vx + 2*b[1].weight*b[1].vx)/(b[0].weight + b[1].weight));
-            double newb0vy = ((b[0].weight*b[0].vy - b[1].weight*b[0].vy + 2*b[1].weight*b[1].vy)/(b[0].weight + b[1].weight));
+			double newb0vy = b[0].vy;
+           // double newb0vy = ((b[0].weight*b[0].vy - b[1].weight*b[0].vy + 2*b[1].weight*b[1].vy)/(b[0].weight + b[1].weight));
             b[1].vx = ((2*b[0].weight * b[0].vx + b[1].weight*b[1].vx - b[0].weight*b[1].vx)/(b[0].weight + b[1].weight));
-            b[1].vy = ((2*b[0].weight * b[0].vy + b[1].weight*b[1].vy - b[0].weight*b[1].vy)/(b[0].weight + b[1].weight));
+            //b[1].vy = ((2*b[0].weight * b[0].vy + b[1].weight*b[1].vy - b[0].weight*b[1].vy)/(b[0].weight + b[1].weight));
 
             b[0].vx = Math.cos(-angle) * newb0vx - Math.sin(-angle) * newb0vy;
             b[0].vy = Math.cos(-angle) * newb0vy + Math.sin(-angle) * newb0vx;
